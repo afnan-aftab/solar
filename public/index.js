9d99788120159e0329a64be5240c104ba73ebfbd
afnan-aftab <aftabafnan2000@gmail.com>
2020-12-08 20:07:59 +0500

    dkd

---
+++ public/index.js
@@ -0,0 +1,19 @@
+// // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+var firebaseConfig = {
+    apiKey: "AIzaSyBKpj34iA8Slz69O2EdFikdZURdlAPo7RA",
+    authDomain: "iot-solar-database.firebaseapp.com",
+    databaseURL: "https://iot-solar-database-default-rtdb.firebaseio.com",
+    projectId: "iot-solar-database",
+    storageBucket: "iot-solar-database.appspot.com",
+    messagingSenderId: "804706405547",
+    appId: "1:804706405547:web:10a3358b308bc4c891fbee",
+    measurementId: "G-Q18KF5TDXH"
+  };
+// Initialize Firebase with a "default" Firebase project
+var Project = firebase.initializeApp(firebaseConfig);
+console.log(Project.name);
+var database = firebase.database();
+// // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+  
+
