b5a3bd896935c93be8fb8135713d63f84a026004
afnan-aftab <aftabafnan2000@gmail.com>
2020-12-11 02:13:00 +0500

    power-plant-page-update

--- public/index.js
+++ public/index.js
@@ -17,7 +17,8 @@ var aut = firebase.auth();
 // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
 
 
-var snippet = "<tr><th scope=\"row\">"+"{{KEY}}"+"</th><td>"+"{{TTTT}}"+"</td><td>"+"{{AXXX}}"+"</td><td>"+"{{AYYY}}"+"</td><td>"+"{{AZZZ}}"+"</td></tr>";
+var snippet_gyro = "<tr><th scope=\"row\">"+"{{KEY}}"+"</th><td>"+"{{TTTT}}"+"</td><td>"+"{{AXXX}}"+"</td><td>"+"{{AYYY}}"+"</td><td>"+"{{AZZZ}}"+"</td></tr>";
+var snippet_power = "<tr><th scope=\"row\">"+"{{KEY}}"+"</th><td>"+"{{TTTT}}"+"</td><td>"+"{{AXXX}}"+"</td><td>"+"{{AYYY}}"+"</td><td>"+"{{AZZZ}}"+"</td><td>"+"{{AGGG}}"+"</td></tr>";
 var data_row_array = [];
 
 document.addEventListener("DOMContentLoaded", initial);
@@ -26,20 +27,75 @@ function initial(){
 
 }
 
-function add_row(T,Ax,Ay,Az,key){
-  var h = snippet.replace("{{TTTT}}",T);
+function add_row_gyro(T,Ax,Ay,Az,key){
+  var h = snippet_gyro.replace("{{TTTT}}",T);
   h = h.replace("{{KEY}}",key);
   h = h.replace("{{AXXX}}",Ax);
   h = h.replace("{{AYYY}}",Ay);
   h = h.replace("{{AZZZ}}",Az);
-  if(document.getElementById('test')==null)
+  if(document.getElementById('table-gyro')==null)
   {}else{
-    document.getElementById('test').insertAdjacentHTML("beforeend", h);
+    document.getElementById('table-gyro').insertAdjacentHTML("beforeend", h);
   }
   
 }
 
+function add_row_power(key,date,AC_p,CD_p,dailyyield,Tot){
+  var h = snippet_power.replace("{{KEY}}",key);
+  h = h.replace("{{TTTT}}",date);
+  h = h.replace("{{AXXX}}",AC_p);
+  h = h.replace("{{AYYY}}",CD_p);
+  h = h.replace("{{AZZZ}}",dailyyield);
+  h = h.replace("{{AGGG}}",Tot);
+  if(document.getElementById('table-pp')==null)
+  {}else{
+    document.getElementById('table-pp').insertAdjacentHTML("beforeend", h);
+  }
+  
+}
+
+function power_chart(arr,id_div){
+  google.charts.load('current', {'packages':['scatter']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart () {
+
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Date & Time');
+        data.addColumn('number', 'AC Power');
+        data.addColumn('number', 'DC Power');
+        data.addColumn('number', 'Daily Yield');
+
+
+        data.addRows(arr);
+
+        var options = {
+          width: 800,
+          height: 500,
+          chart: {
+            title: 'AC & DC Power/Dai;y Yield vs Date and Time',
+            subtitle: 'Scatter Plot'
+          },
+          series: {
+            0: {axis: 'AC/DC Power'},
+            1: {axis: 'Daily Yield'}
+          },
+          axes: {
+            y: {
+              'Power': {label: 'AC/DC'},
+              'Daily Yield': {label: 'Yield'}
+            }
+          }
+        };
+
+        var chart = new google.charts.Scatter(document.getElementById(id_div));
+
+        chart.draw(data, google.charts.Scatter.convertOptions(options));
+      }
+}
+
 var query = database.ref('Room1').orderByKey();
+var querypower = database.ref('power').orderByChild('DATE_TIME');
 
 query.on('value', (snapshot) => {
   snapshot.forEach(function(childSnapshot){
@@ -49,7 +105,7 @@ query.on('value', (snapshot) => {
     var Acz = childSnapshot.val().Az;
     var T = childSnapshot.val().Temp;
     
-    add_row(T,Acx,Acy,Acz,key);
+    add_row_gyro(T,Acx,Acy,Acz,key);
   });
 });
 
@@ -61,9 +117,10 @@ query.limitToLast(1).on('child_added', (childSnapshot) => {
     var Acz = childSnapshot.val().Az;
     var T = childSnapshot.val().Temp;
     
-    add_row(T,Acx,Acy,Acz,key);
+    add_row_gyro(T,Acx,Acy,Acz,key);
  
 });
+
 
 $(document).ready(function(){
   $("#rm_but").click(function(){
@@ -75,6 +132,43 @@ $(document).ready(function(){
 $(document).ready(function(){
   $("#login").click(function(){
     $("#loginModal").modal('toggle')
+  });
+
+});
+
+$(document).ready(function(){
+  $("#show_table_power").click(function(){
+    querypower.on('value', (snapshot) => {
+      snapshot.forEach(function(childSnapshot){
+        var key = childSnapshot.key;
+        var AC_p = childSnapshot.val().AC_POWER;
+        var CD_p = childSnapshot.val().DC_POWER;
+        var dailyyield = childSnapshot.val().DAILY_YIELD;
+        var Tot = childSnapshot.val().TOTAL_YIELD;
+        var date = childSnapshot.val().DATE_TIME;
+        
+        add_row_power(key,date,AC_p,CD_p,dailyyield,Tot);
+      });
+    });
+    document.getElementById('show_table_power').innerHTML = "Click to Refresh Data";
+  });
+
+});
+
+$(document).ready(function(){
+  $("#show_plot_power").click(function(){
+    var arr = [];
+    querypower.on('value', (snapshot) => {
+      snapshot.forEach(function(childSnapshot){
+        var AC_p = childSnapshot.val().AC_POWER;
+        var CD_p = childSnapshot.val().DC_POWER;
+        var dailyyield = childSnapshot.val().DAILY_YIELD;
+        var date = childSnapshot.val().DATE_TIME;
+        arr[arr.length]=[date,AC_p,CD_p,dailyyield];
+      });
+    });
+    power_chart(arr,'chart_power');
+    document.getElementById('show_plot_power').innerHTML = "Click to Refresh Chart";
   });
 
 });
