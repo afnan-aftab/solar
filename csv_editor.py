2e1c0a9cebdc34e1480584c98618c2a88dc0fa13
afnan-aftab <aftabafnan2000@gmail.com>
2020-12-16 00:04:00 +0500

    python-firebase-success

--- csv_editor.py
+++ csv_editor.py
@@ -1,5 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import db
 
 cred = credentials.Certificate("admin-sdk.json")
-firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app(cred,{
+    'databaseURL': 'https://iot-solar-database-default-rtdb.firebaseio.com/'
+})
+
+ref = db.reference('Room2/')
+ref.set({})
