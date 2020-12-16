cf4e75e6f15cd776f0e673c26fcbe378ba9e1f13
afnan-aftab <aftabafnan2000@gmail.com>
2020-12-16 18:35:23 +0500

    final-edited-csv

--- csv_editor.py
+++ csv_editor.py
@@ -1,12 +1,20 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+from firebase_admin import storage
 import pandas
 
 cred = credentials.Certificate("admin-sdk.json")
 firebase_admin.initialize_app(cred,{
-    'databaseURL': 'https://iot-solar-database-default-rtdb.firebaseio.com/'
+    'databaseURL': 'https://iot-solar-database-default-rtdb.firebaseio.com/',
+    'storageBucket':'iot-solar-database.appspot.com'
+})
+
+bucket = storage.bucket()
+
+ref = db.reference('Room2/')
+ref.set({
+    'family':0
 })
 
 df = pandas.read_csv('dataset-solar/Plant_1_Generation_Data.csv')
-
