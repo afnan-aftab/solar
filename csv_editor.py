03e7e00cc84ab343e255e343ea3f55e22bb3fc52
afnan-aftab <aftabafnan2000@gmail.com>
2020-12-16 14:43:21 +0500

    python-error-free

--- csv_editor.py
+++ csv_editor.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+import pandas
 
 cred = credentials.Certificate("admin-sdk.json")
 firebase_admin.initialize_app(cred,{
@@ -9,3 +10,6 @@ firebase_admin.initialize_app(cred,{
 
 ref = db.reference('Room2/')
 ref.set({})
+
+df = pandas.read_csv('dataset-solar/Plant_1_Generation_Data.csv')
+print(df)
